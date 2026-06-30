2de639b46f4008e27d8c1de12760fcaad7aa6305
Naplon_ <94738563+naplon74@users.noreply.github.com>
2026-06-30 13:29:49 +0200

    Add files via upload

---
+++ registry_tools/offline_os_version_reg.bat
@@ -0,0 +1,10 @@
+@echo off
+
+rem This version tells you the version and the edition of Windows installed on the drive.
+rem Note that this will only work if you are running the script from a Windows PE environment. 
+rem It checks for OFFLINE registry hive, so it will not work if you are running this script on a normal Windows installation. If you want to check the version of the currently running Windows, use os_version_local.bat instead.
+rem Check HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion if for some reason you want to check the version of the currently running Windows. Tho I run Windows 11 and it says Windows 10 Pro in the registry
+
+reg load HKLM\OFFLINE C:\Windows\System32\Config\SOFTWARE
+reg query "HKLM\OFFLINE\Microsoft\Windows NT\CurrentVersion" /v ProductName
+reg unload HKLM\OFFLINE
